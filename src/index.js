111da49bba9693b423ce7c86e49b58a1106e7309
Drakodin <talia.yl.tian@gmail.com>
2022-01-31 11:13:42 -0500

    Add default heights, fix page scrolling.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 /**
  * Scrolls the browser window to the element with the given
  * id attribute, setting its relative location to be on top.
- * @param {MouseEvent} event The event triggered by a click 
  */
-function scrollToElement(event) {
+function scrollToElement() {
     // Find and remove the last "active" nav element, if exists
     let lastActive = getLastActive();
     if (lastActive) {
         lastActive.classList.remove('active');
     }
 
-    event.target.classList.add('active');
-    let id = event.target.getAttribute('data-target')
+    window.event.target.classList.add('active');
+    let id = window.event.target.getAttribute('data-target')
     let el = document.getElementById(id);
-    let top = el.getBoundingClientRect().top + window.pageYOffset;
+    let nav = document.querySelector('nav');
+    let top = el.getBoundingClientRect().top + window.pageYOffset - nav.offsetHeight - 4;
 
     window.scrollTo({
         top: top,
