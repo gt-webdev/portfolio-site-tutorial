c99d4d3a2e2cd73c2a20b76765a4700ddc860e38
Drakodin <talia.yl.tian@gmail.com>
2022-01-31 09:32:29 -0500

    Add navbar.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 /**
  * Scrolls the browser window to the element with the given
  * id attribute, setting its relative location to be on top.
- * @param {string} id 
+ * @param {MouseEvent} event The event triggered by a click 
  */
-function scrollTo(id) {
+function scrollToElement(event) {
+    // Find and remove the last "active" nav element, if exists
+    let lastActive = getLastActive();
+    if (lastActive) {
+        lastActive.classList.remove('active');
+    }
+
+    event.target.classList.add('active');
+    let id = event.target.getAttribute('data-target')
     let el = document.getElementById(id);
     let top = el.getBoundingClientRect().top + window.pageYOffset;
 
@@ -11,4 +19,8 @@ function scrollTo(id) {
         top: top,
         behavior: 'smooth'
     });
+}
+
+function getLastActive() {
+    return document.querySelector('.active');
 }
